7340b4cca810852303f69d2d22a7ce30e915eadc
Youssef Bahgat <62761039+itsbahgat@users.noreply.github.com>
2025-02-09 22:47:11 +0200

    Fix import paths for Arrow icon across multiple components

--- components/auth/Signup/index.tsx
+++ components/auth/Signup/index.tsx
@@ -14,7 +14,7 @@ import { router } from "expo-router";
 import i18n from "../../../i18n/i18n";
 import styles from "./signupStyle";
 import Button from "@/commonComponent/button/Button";
-import Arrow from "../../../assets/icons/Arrow.svg";
+import Arrow from "../../../assets/icons/arrow.svg";
 import { Formik, FormikHelpers } from "formik";
 import * as Yup from "yup";
 import { useDispatch, UseDispatch } from "react-redux";
